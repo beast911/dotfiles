e6ac8e50b9c27367a7dead44dde32b9ba224d634
Kanwar Singh <ks2591@gmail.com>
2023-10-13 11:48:02 +0200

    add rainbow indentline

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,7 +29,7 @@ opt.numberwidth = 2
 opt.ruler = false
 
 -- disable nvim intro
-opt.shortmess:append "sI"
+opt.shortmess:append("sI")
 
 opt.signcolumn = "yes"
 opt.splitbelow = true
@@ -43,23 +43,23 @@ opt.updatetime = 250
 
 -- go to previous/next line with h,l,left arrow and right arrow
 -- when cursor reaches end/beginning of line
-opt.whichwrap:append "<>[]hl"
+opt.whichwrap:append("<>[]hl")
 
 g.mapleader = " "
 
 -- disable some default providers
-for _, provider in ipairs { "node", "perl", "python3", "ruby" } do
-  vim.g["loaded_" .. provider .. "_provider"] = 0
+for _, provider in ipairs({ "node", "perl", "python3", "ruby" }) do
+	vim.g["loaded_" .. provider .. "_provider"] = 0
 end
 
 -- add binaries installed by mason.nvim to path
 local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
-vim.env.PATH = vim.fn.stdpath "data" .. "/mason/bin" .. (is_windows and ";" or ":") .. vim.env.PATH
+vim.env.PATH = vim.fn.stdpath("data") .. "/mason/bin" .. (is_windows and ";" or ":") .. vim.env.PATH
 require("config.keymaps")
 require("config.lazy")
 require("config.telescope")
 -- themeing --
-vim.g.sonokai_style = 'shusia'
+vim.g.sonokai_style = "shusia"
 vim.g.sonokai_better_performance = 1
 vim.g.everforest_diagnostic_line_highlight = 1
-vim.cmd("colorscheme ayu-dark")
+vim.cmd("colorscheme tokyonight-night")
--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -8,61 +8,103 @@ end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
-  spec = {
-    {
-      "Shatur/neovim-ayu",
-      priority = 1000
-    },
-    {
-      "neanias/everforest-nvim",
-      version = false,
-      lazy = false,
-      priority = 1000, -- make sure to load this before all the other start plugins
-    },
-    {
-      "sainnhe/sonokai",
-      priority = 1000
-    },
-    {
-      "catppuccin/nvim",
-      name = "catppuccin",
-      priority = 1000,
-    },
-    {
-      "folke/tokyonight.nvim",
-      lazy = false,
-      priority = 1000,
-      opts = {},
-      config = function()
-        require("tokyonight").setup {}
-      end
-    },
-    { import = "plugins" },
-  },
-  defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
-  },
-  install = { colorscheme = { "catppuccin", "tokyonight", "habamax", "monokai" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
-  performance = {
-    rtp = {
-      -- disable some rtp plugins
-      disabled_plugins = {
-        "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
+	spec = {
+		{
+			"Shatur/neovim-ayu",
+			priority = 1000,
+		},
+		{
+			"neanias/everforest-nvim",
+			version = false,
+			lazy = false,
+			priority = 1000, -- make sure to load this before all the other start plugins
+		},
+		{
+			"sainnhe/sonokai",
+			priority = 1000,
+		},
+		{
+			"catppuccin/nvim",
+			name = "catppuccin",
+			priority = 1000,
+		},
+		{
+			"folke/tokyonight.nvim",
+			lazy = false,
+			priority = 1000,
+			opts = {},
+			config = function()
+				require("tokyonight").setup({
+					-- use the night style
+					style = "night",
+					-- disable italic for functions
+					styles = {
+						functions = {},
+					},
+					on_highlights = function(hl, c)
+						local prompt = "#2d3149"
+						hl.TelescopeNormal = {
+							bg = c.bg_dark,
+							fg = c.fg_dark,
+						}
+						hl.TelescopeBorder = {
+							bg = c.bg_dark,
+							fg = c.bg_dark,
+						}
+						hl.TelescopePromptNormal = {
+							bg = prompt,
+						}
+						hl.TelescopePromptBorder = {
+							bg = prompt,
+							fg = prompt,
+						}
+						hl.TelescopePromptTitle = {
+							bg = prompt,
+							fg = prompt,
+						}
+						hl.TelescopePreviewTitle = {
+							bg = c.bg_dark,
+							fg = c.bg_dark,
+						}
+						hl.TelescopeResultsTitle = {
+							bg = c.bg_dark,
+							fg = c.bg_dark,
+						}
+					end,
+					sidebars = { "qf", "vista_kind", "terminal", "packer" },
+					-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+					on_colors = function(colors)
+						colors.bg = "#000000"
+					end,
+				})
+			end,
+		},
+		{ import = "plugins" },
+	},
+	defaults = {
+		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+		-- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+		lazy = false,
+		-- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+		-- have outdated releases, which may break your Neovim install.
+		version = false, -- always use the latest git commit
+		-- version = "*", -- try installing the latest stable version for plugins that support semver
+	},
+	install = { colorscheme = { "catppuccin", "tokyonight", "habamax", "monokai" } },
+	checker = { enabled = true }, -- automatically check for plugin updates
+	performance = {
+		rtp = {
+			-- disable some rtp plugins
+			disabled_plugins = {
+				"gzip",
+				-- "matchit",
+				-- "matchparen",
+				-- "netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 })
