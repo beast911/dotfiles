0378281f7cfc03ad7ec5d6a93f3416d6ff1c4b25
Kanwar Singh <ks2591@gmail.com>
2023-10-13 10:05:20 +0200

    remove rainbow indentline

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -15,6 +15,6 @@ end
 -- For example, changing the color scheme:
 config.color_scheme = "3024 (dark) (terminal.sexy)"
 config.font_size = 16.0
-config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
+config.font = wezterm.font("JetBrains Mono", {})
 -- and finally, return the configuration to wezterm
 return config
