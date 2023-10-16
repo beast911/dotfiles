059f815808872d42d025c3ae2af1368ff65c6e72
Kanwar Singh <ks2591@gmail.com>
2023-10-16 15:03:24 +0200

    add dap ui

---
+++ nvim/lua/plugins/debugger.lua
@@ -0,0 +1,14 @@
+local keymap = require("config.keymaps").map
+return {}
+-- return {
+-- 	"rcarriga/nvim-dap-ui",
+-- 	lazy = false,
+-- 	dependencies = {
+-- 		"mfussenegger/nvim-dap",
+-- 	},
+-- 	config = function()
+--     require("dapui").setup()
+-- 		local dapui = require("dapui")
+-- 		keymap("n", "<space>do", dapui.open())
+-- 	end,
+-- }
