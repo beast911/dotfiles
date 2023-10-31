2f8887575acc7bfa1f873ffea7111046547d374a
Kanwar Singh <ks2591@gmail.com>
2023-10-31 09:28:28 +0100

    fix null ls

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,96 +1,56 @@
-local async_formatting = function(bufnr)
-  bufnr = bufnr or vim.api.nvim_get_current_buf()
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{
-				"lukas-reineke/lsp-format.nvim",
-			},
-		},
-		config = function()
-			local M = {}
-			M.capabilities = vim.lsp.protocol.make_client_capabilities()
+return {
+	"jose-elias-alvarez/null-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = { "mason.nvim" },
+	config = function()
+		-- custom/configs/null-ls.lua
 
-			M.capabilities.textDocument.completion.completionItem = {
-				documentationFormat = { "markdown", "plaintext" },
-				snippetSupport = true,
-				preselectSupport = true,
-				insertReplaceSupport = true,
-				labelDetailsSupport = true,
-				deprecatedSupport = true,
-				commitCharactersSupport = true,
-				tagSupport = { valueSet = { 1 } },
-				resolveSupport = {
-					properties = {
-						"documentation",
-						"detail",
-						"additionalTextEdits",
-					},
+		local null_ls = require("null-ls")
+
+		local formatting = null_ls.builtins.formatting
+		local lint = null_ls.builtins.diagnostics
+		local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+		local event = "BufWritePre" -- or "BufWritePost"
+		local async = event == "BufWritePost"
+
+		local sources = {
+			formatting.prettier.with({
+				env = {
+					PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/git_clones/flights/frontend/.prettierrc"),
 				},
-			}
+			}),
+			formatting.stylua,
+			-- lint.shellcheck,
+		}
 
-			-- Global mappings.
-			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
-			keymap("n", "<space>e", vim.diagnostic.open_float)
-			keymap("n", "[d", vim.diagnostic.goto_prev)
-			keymap("n", "]d", vim.diagnostic.goto_next)
-			vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+		null_ls.setup({
+			debug = false,
+			sources = sources,
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.keymap.set("n", "<Leader>f", function()
+						vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+					end, { buffer = bufnr, desc = "[lsp] format" })
 
-			-- Use LspAttach autocommand to only map the following keys
-			-- after the language server attaches to the current buffer
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					-- Enable completion triggered by <c-x><c-o>
-					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+					-- format on save
+					vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						group = group,
+						callback = function()
+							-- async_formatting(bufnr)
+							vim.lsp.buf.format({ bufnr = bufnr, timeout_ms = 2000 })
+						end,
+						desc = "[lsp] format on save",
+					})
+				end
 
-					-- Buffer local mappings.
-					-- See `:help vim.lsp.*` for documentation on any of the below functions
-					local opts = { buffer = ev.buf }
-					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-					vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-					vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-					vim.keymap.set("n", "<space>wl", function()
-						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-					end, opts)
-					vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-					vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-					vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-					vim.keymap.set("n", "<space>f", function()
-						vim.lsp.buf.format({ async = true })
-					end, opts)
-				end,
-			})
-
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				on_attach = M.on_attach,
-			})
-			lspconfig.tsserver.setup({
-				on_attach = M.on_attach,
-				capabilities = M.capabilities,
-
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
-						workspace = {
-							library = {
-								[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-								[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-								[vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy"] = true,
-							},
-							maxPreload = 100000,
-							preloadFileSize = 10000,
-						},
-					},
-				},
-			})
-		end,
-	
+				if client.supports_method("textDocument/rangeFormatting") then
+					vim.keymap.set("x", "<Leader>f", function()
+						vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+					end, { buffer = bufnr, desc = "[lsp] format" })
+				end
+			end,
+		})
+	end,
+}
